f051a5d0545fe1697f56e97727fdbb919ab27b67
Marcel Neumann <marcel.neumann@rewe-digital.com>
2020-10-27 00:08:16 +0100

    Extract post method in new parent class TrainDataServiceTest

--- train_data_service/test_train_data_service.py
+++ train_data_service/test_train_data_service.py
@@ -8,7 +8,15 @@ from cherrypy.test import helper
 from train_data_service.train_data_service import TrainDataService
 
 
-class TrainDataServiceEmptyTrainTest(helper.CPWebCase):
+class TrainDataServiceTest(helper.CPWebCase):
+    def post(self, url, body):
+        self.getPage(url=url,
+                     method='POST',
+                     headers=[('Content-Type', 'application/json'), ('Content-Length', str(len(body)))],
+                     body=body)
+
+
+class EmptyTrainTest(TrainDataServiceTest):
     @staticmethod
     def setup_server():
         cherrypy.tree.mount(TrainDataService(json.dumps(
@@ -19,37 +27,25 @@ class TrainDataServiceEmptyTrainTest(helper.CPWebCase):
         self.assertInBody('1A')
 
     def test_reserve_seat(self):
-        body = json.dumps({'train_id': 'foo_train', 'seats': ['1A'], 'booking_reference': '01234567'})
-        self.getPage('/reserve',
-                     method='POST',
-                     headers=[('Content-Type', 'application/json'), ('Content-Length', str(len(body)))],
-                     body=body)
+        self.post('/reserve', json.dumps({'train_id': 'foo_train', 'seats': ['1A'], 'booking_reference': '01234567'}))
         self.assertInBody('"booking_reference": "01234567"')
 
 
-class TrainDataServiceReservedTrainTest(helper.CPWebCase):
+class ReservedTrainTest(TrainDataServiceTest):
     @staticmethod
     def setup_server():
         cherrypy.tree.mount(TrainDataService(json.dumps(
             {'foo_train': {'seats': {'1A': {'coach': 'A', 'seat_number': '1', 'booking_reference': 'existing'}}}})))
 
     def test_reserve_seat_when_already_reserved(self):
-        body = json.dumps({'train_id': 'foo_train', 'seats': ['1A'], 'booking_reference': '01234567'})
-        self.getPage('/reserve',
-                     method='POST',
-                     headers=[('Content-Type', 'application/json'), ('Content-Length', str(len(body)))],
-                     body=body)
+        self.post('/reserve', json.dumps({'train_id': 'foo_train', 'seats': ['1A'], 'booking_reference': '01234567'}))
         self.assertInBody('already booked with reference: existing')
 
         self.getPage('/data_for_train/foo_train')
         self.assertInBody('"booking_reference": "existing"')
 
     def test_reserve_with_typo_in_seat_id(self):
-        body = json.dumps({'train_id': 'foo_train', 'seats': ['typo'], 'booking_reference': '01234567'})
-        self.getPage('/reserve',
-                     method='POST',
-                     headers=[('Content-Type', 'application/json'), ('Content-Length', str(len(body)))],
-                     body=body)
+        self.post('/reserve', json.dumps({'train_id': 'foo_train', 'seats': ['typo'], 'booking_reference': '01234567'}))
         self.assertInBody('seat not found typo')
 
     def test_reset(self):
